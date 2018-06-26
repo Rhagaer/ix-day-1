36e2c08f6646c84b451bbacd9b3073b16ebd925f
Tamir Shklaz <shklazt@gmail.com>
2018-06-26 12:33:25 +0200

    Added Navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LoginPage } from '../login/login';
+import { RegistrationPage } from '../registration/registration';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +9,19 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  variable1: string;
+
   constructor(public navCtrl: NavController) {
 
+  }
+
+  navigateToLogin = () => {
+    console.log("Navigating")
+    this.navCtrl.push(LoginPage)
+  }
+
+  navigateToRegister = () => {
+    this.navCtrl.push(RegistrationPage);
   }
 
 }
---
+++ src/pages/registration/registration.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { ProfilePage } from '../profile/profile';
+
+/**
+ * Generated class for the RegistrationPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-registration',
+  templateUrl: 'registration.html',
+})
+export class RegistrationPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegistrationPage');
+  }
+
+  navigateToProfile = () => {
+    this.navCtrl.push(ProfilePage)
+  }
+
+}
